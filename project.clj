34783742009aa000dff564887b245e2ccc188809
Harry Binnendijk <harry@empanda.net>
2013-12-25 14:48:53 +1100

    with rohm

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [org.clojure/clojurescript "0.0-2127"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.1.0-SNAPSHOT"]
-                 [ohm "0.1.0-SNAPSHOT"]
+                 [rohm "0.1.0-SNAPSHOT"]
                  ]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
--- src/test_om/core.cljs
+++ src/test_om/core.cljs
@@ -1,19 +1,18 @@
 (ns test-om.core
-  (:require-macros [cljs.core.async.macros :refer [go alt!]]
-                   [ohm.core :as ohm ])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! <! chan]]
             [test-om.utils :refer [guid]]
             [clojure.browser.net :as net]
             [clojure.browser.event :as gevent]
+            [rohm.core :as rohm :include-macros true] 
             ))
 
 ;; state
 (def app-state (atom {:url "comments.json" :poll-interval 4000 
                       :comments [{:id (guid) :author "a1" :text "t1"} {:id (guid) :author "a2" :text "t2"}]}))
 
-;; event handlers
+;; Pedestal style input message transformer functions
 (defn start-edit [old-value message]
   (assoc old-value :editing true))
 
@@ -24,44 +23,20 @@
   ;(.info js/console (pr-str "add-comment " old-value message))
   (conj old-value {:id (guid) :author author :text text}))
 
+;; Pedestal style routes
 (def routes [[:start-edit [:comments :*] start-edit]
              [:update-comment [:comments :*] update-comment]
              [:add-comment [:comments] add-comment]
              ])
-
-;; TODO move to ohm
-(def input-queue (chan))
-
-(defn de-route [type topic]
-  ; TODO implement topic based
-  (-> (filter #(= (first %) type) routes) first last))
-
-(defn handle-mesg [{:keys [type topic] :as mesg}]
-  (let [old-value (get-in @app-state topic)]
-    (if-let [func (de-route type topic)]
-      (let [new-value (func old-value mesg)]
-        (swap! app-state update-in topic (fn [_ nv] nv) new-value)
-        )
-      (.warn js/console (pr-str "handle-mesg: no func for " type topic)))))
-
-(defn put-msg [type cursor & opts]
-  ;(.info js/console (pr-str "put-msg " type cursor opts))
-  (put! input-queue (merge {:type type :topic (:om.core/path (meta cursor))} opts)))
-
-(defn extract-refs [owner]
-  (let [ks (keys (js->clj (.-refs owner)))
-        m (into {} (map #(vector (keyword %) (.-value (om/get-node owner %))) ks))]
-    ;(.info js/console (pr-str "handle-submit " ks m))
-    m))
 
 ;; components
 (def ENTER-KEY 13)
 
 (defn comment-form [comments]
   (letfn [(handle-submit [_ owner]
-            (put-msg :add-comment comments (extract-refs owner))
+            (rohm/put-msg :add-comment comments (rohm/extract-refs owner))
             false)]
-    (ohm/component-o
+    (rohm/component-o
       (dom/form #js {:className "commentForm" :onSubmit #(handle-submit % owner)}
                 (dom/input #js {:ref "author" :type "text" :placeholder "Your name" })
                 (dom/input #js {:ref "text" :type "text" :placeholder "Say something..."})
@@ -71,10 +46,10 @@
   (letfn [(update [e cmt owner] 
             (when (= (.-which e) ENTER-KEY)
               (let [node (om/get-node owner "edit")]
-                (put-msg :update-comment cmt {:author (.-value node)}))))]
-    (ohm/component-o
+                (rohm/put-msg :update-comment cmt {:author (.-value node)}))))]
+    (rohm/component-o
       (dom/div #js {:className "comment"}
-               (dom/h2 #js {:ref "author" :onClick #(put-msg :start-edit cmt) } 
+               (dom/h2 #js {:ref "author" :onClick #(rohm/put-msg :start-edit cmt) } 
                        (if-not (:editing cmt) 
                          (:author cmt)
                          (dom/input #js {:ref "edit" :onKeyDown #(update % cmt owner) :defaultValue (:author cmt)})))
@@ -83,12 +58,19 @@
 (defn comment-list [comments]
   (om/component
     (dom/div #js {:className "commentList"}
-             (ohm/list-of comment comments))))
+             (rohm/list-of comment comments))))
 
 (defn fail [ev]
   (.error js/console "fail: " ev))
 
-(defn comment-box [app]
+(defn comment-box [comments]
+  (om/component
+    (dom/div #js {:className "commentBox"} 
+             (dom/h1 nil "Comments")
+             (om/build comment-list comments)
+             (om/build comment-form comments))))
+
+(defn comment-app [app]
   ; TODO put in service
   (letfn [(server-res [ev]
             (let [res (js->clj (.getResponseJson (.-target ev)) :keywordize-keys true)
@@ -105,13 +87,9 @@
         (let [{:keys [url poll-interval]} app]
           (get-from-server url)
           (js/setInterval get-from-server poll-interval url)
-          (go (while true
-                (handle-mesg (<! input-queue))))))
+          (rohm/handle-messages app-state routes)))
       om/IRender
       (render [_ _]
-        (dom/div #js {:className "commentBox"} 
-                 (dom/h1 nil "Comments")
-                 (om/build comment-list app {:path [:comments]})
-                 (om/build comment-form app {:path [:comments]}))))))
+        (om/build comment-box app {:path [:comments]})))))
 
-(om/root app-state comment-box (.getElementById js/document "container"))
+(om/root app-state comment-app (.getElementById js/document "container"))
